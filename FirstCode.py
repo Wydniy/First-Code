45d936d991d7de85a80251659f0b4a4d6f8fc84d
Janiyah Sherrill <164689300+Wydniy@users.noreply.github.com>
2024-04-12 18:07:10 -0400

    Add files via upload

---
+++ FirstCode.py
@@ -0,0 +1,11 @@
+print("Hello! What is your name?")
+name = "Janiyah"
+print(f"Nice to meet you, {name}!")
+print("How old are you?")
+age = 17
+bot_age = 3
+age_difference = age - bot_age
+print (f"You are {age_difference} years olfer than me. Im only {bot_age} years old!")
+print ("What's your favorite color?")
+color = "Black"
+print (f"Oh, {color} is a beautiful color!")
